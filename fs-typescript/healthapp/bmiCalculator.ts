5452d5d9963444cfda11860505fac851fe83327e
kuol buom <kuolbuom111@gmail.com>
2026-08-19 15:48:38 +0300

    exercise: 3 command-line bmiCalculator

--- fs-typescript/healthapp/bmiCalculator.ts
+++ fs-typescript/healthapp/bmiCalculator.ts
@@ -1,3 +1,5 @@
+import { isNotNumber } from "./utils.ts";
+
 const calculateBmi = (height: number, weight: number) => {
   const heightInMeter = height / 100;
   const bmi = weight / (heightInMeter * heightInMeter);
@@ -12,4 +14,34 @@ const calculateBmi = (height: number, weight: number) => {
     return "obese";
   }
 };
-console.log(calculateBmi(180, 74));
+
+//command-line
+try {
+  //this split out "node" and "calculateBmi" and leave only two arguments like "180" and "91"
+  const args = process.argv.slice(2);
+
+  //arguments length must be only two no more or less
+  if (args.length < 2) {
+    throw new Error("Please provide an arguments");
+  }
+
+  //checks whether extra. fore example, "node","bmiCalculator.ts", "180", "91","extra"
+  if (args.length > 4) {
+    throw new Error("Too many arguments");
+  }
+
+  //this some isNotNuber prevent any argument(s) which is not a number
+  if (args.some(isNotNumber)) {
+    throw new Error("Arguments must be numbers");
+  }
+
+  //remaining from slice are now converted to numbers
+  const height = Number(args[0]);
+  const weight = Number(args[1]);
+
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  if (error instanceof Error) {
+    console.log("Error:", error.message);
+  }
+}
