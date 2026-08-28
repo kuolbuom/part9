6a7623fb503f3f5392388f44906cf50146bfa5aa
kuol buom <kuolbuom111@gmail.com>
2026-08-28 15:02:15 +0300

    Exercise: 25 Patientor, step3

--- fs-typescript/patientor/backend/src/types.ts
+++ fs-typescript/patientor/backend/src/types.ts
@@ -28,7 +28,40 @@ export type NewPatient = z.infer<typeof NewPatientSchema>;
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 
-export interface Entry {}
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: string[];
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: number;
+}
+
+export type Entry =
+  | OccupationalHealthcareEntry
+  | HospitalEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
